0f32aa53dd7e24e3e59cd3cbe00ae84a6fbaf3f5
Michael Gerland <mr.gerland@gmail.com>
2019-11-04 22:41:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)# code an argument here
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
 end
 

03b34b9d14d402f355c3615cd309912b7dc7a0bc
Michael Gerland <mr.gerland@gmail.com>
2019-11-04 22:30:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(array)# code an argument here
-  array.each_with_index {|item, index| puts "#{index}#{item}"
+  array.each_with_index {|item, index| puts "#{array}"
   # Your code here
 end
 
